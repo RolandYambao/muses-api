f283abda996ceff76839efa41f6b539d93e3ea54
RolandYambao <rolandyambao@gmail.com>
2022-01-27 15:58:54 -0800

    Updated

--- models/user.js
+++ models/user.js
@@ -1,10 +1,17 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
 
+const portfolioSchema = new mongoose.Schema({
+    pictureUrl: String,
+    title: String,
+    description: String,
+})
+
 const userSchema = new mongoose.Schema({
     name: String,
     email: String,
     password: String,
+    portfolio: [portfolioSchema]
 });
 
 const User = mongoose.model('User', userSchema);
