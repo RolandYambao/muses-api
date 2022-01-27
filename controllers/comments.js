5c18bc9278206ca458c6c51cdbe33113d18aecf0
RolandYambao <rolandyambao@gmail.com>
2022-01-27 13:54:09 -0800

    Updated

--- controllers/comments.js
+++ controllers/comments.js
@@ -17,7 +17,7 @@ router.get('/', function (req, res) {
 });
 
 // Post Routes
-router.post('/', passport.authenticate('jwt', { session: false }), (req, res) => {
+router.post('/', function (req, res) {
     Comment.create({
         content: req.body.content,
     })
@@ -30,35 +30,5 @@ router.post('/', passport.authenticate('jwt', { session: false }), (req, res) =>
             res.json('Error occured, please try again....');
         });
 });
-
-// // Edit
-// router.put('/:id', passport.authenticate('jwt', { session: false }), (req, res) => {
-//     let commentIndex = Number(req.params.id);
-//     Comment.findByIdAndUpdate({
-//         content: req.body.content,
-//     }, { where: { id: commentIndex } })
-//         .then(function (response) {
-//             console.log('Edited Comment', response);
-//             res.json(response);
-//         })
-//         .catch(function (error) {
-//             console.log('ERROR', error);
-//             res.json('Error occured, please try again....');
-//         })
-// });
-
-// // Delete
-// router.delete('/:id', passport.authenticate('jwt', { session: false }), (req, res) => {
-//     let commentIndex = Number(req.params.id);
-//     Comment.findByIdAndDelete({ where: { id: commentIndex } })
-//         .then(function (response) {
-//             console.log('Comment Deleted', response);
-//             res.json(response);
-//         })
-//         .catch(function (error) {
-//             console.log('ERROR', error);
-//             res.render('404', { message: 'Update was not successful. Please try again.' });
-//         })
-// });
 
 module.exports = router;
