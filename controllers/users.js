0e16af0b670885fb2db46410c40eb1299320260a
RolandYambao <rolandyambao@gmail.com>
2022-01-28 09:16:59 -0800

    Updated

--- controllers/users.js
+++ controllers/users.js
@@ -30,7 +30,8 @@ router.post('/signup', async (req, res) => {
                 const newUser = new User({
                     name: req.body.name,
                     email: req.body.email,
-                    password: req.body.password
+                    password: req.body.password,
+                    portfolio: [],
                 });
 
                 // Salt and hash the password - before saving the user
